fb258147082c3e99bd254ad62c61c496ad82e4fe
Manabu Ori <manabu.ori@gmail.com>
2023-04-12 23:33:27 +0900

    Add Prometheus exporter support

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import date from "date-utils";
 import ip from "ip";
 import networkInterface from "os";
 import express from "express";
+import promClient from "prom-client";
 
-const portNumber = 8080;
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics({timeout: 5000});
+
+const apiPort = 8080;
 const app = express();
 
 app.get("/", (req, res) => {
@@ -18,5 +22,13 @@ app.get("/", (req, res) => {
     );
 });
 
-app.listen(portNumber, '0.0.0.0');
-console.log(`PortNumber is ${portNumber}`);
+app.listen(apiPort, '0.0.0.0');
+console.log(`[${new Date()}] API endpoint started on ${apiPort}`);
+
+const promPort = 9090;
+const metrics = express();
+metrics.get("/metrics", (req, res) => {
+    res.set("Content-Type", "text/plain");
+    promClient.register.metrics().then(str => res.send(str));
+});
+metrics.listen(promPort, () => console.log(`[${new Date()}] Prometheus exporter started on ${promPort}`));
